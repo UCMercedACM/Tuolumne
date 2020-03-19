0b74fa2011890536c2ae97dec327417799e99ff6
Adrian Darian <adarian@ucmerced.edu>
2020-03-18 23:59:02 -0700

    Workshop database with dev data

--- database/migrations/2020_03_17_000000_create_workshops_table.php
+++ database/migrations/2020_03_17_000000_create_workshops_table.php
@@ -14,7 +14,7 @@ class CreateWorkshopsTable extends Migration
     public function up()
     {
         Schema::create('workshops', function (Blueprint $table) {
-            $table->increments('id');
+            $table->bigIncrements('id');
             $table->string('workshop_name');
             $table->longText('description');
             $table->string('location');
